a60f2e342008e59b74eb2cd4daf2a950039bae22
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2018-09-08 22:41:37 -0300

    Random pick de steams não verificadas

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var request = require('request');
 var callback = "function (data) { document.write(data); document.close(); }" 
-var content = "<button onclick=\"jQuery.get('http://localhost:8089/fake', " + callback + ")\" type='button' style='position:absolute; top:0; background-color: #FF0000; color: #FFFFFF'>É fake!</button><button onclick=\"jQuery.get('http://localhost:8089/next', " + callback + ")\"  type='button' style='position:absolute; top:0; right:50vw; z-index: 500'>Próximo</button><button onclick=\"jQuery.get('http://localhost:8089/confiavel', " + callback + ")\"  type='button' style='position:absolute; top:0; right:0; background-color:#00FF00'>Confiável</button></body></html>"
+var content = "<button onclick=\"jQuery.get('http://localhost:8089/fake', " + callback + ")\" type='button' style='width: 100px; height: 50px; position:absolute; top:0; background-color: #FF0000; color: #FFFFFF'>É fake!</button><button onclick=\"jQuery.get('http://localhost:8089/next', " + callback + ")\"  type='button' style='width: 100px; height: 50px; position:absolute; top:0; right:50vw; z-index: 500'>Próximo</button><button onclick=\"jQuery.get('http://localhost:8089/confiavel', " + callback + ")\"  type='button' style='width: 100px; height: 50px; position:absolute; top:0; right:0; background-color:#00FF00'>Confiável</button></body></html>"
 
 const fileName = "steamid_visitados.json"
 const express = require('express')
@@ -10,12 +10,15 @@ const app = express()
 
 var _idAtual = 0;
 var _keys = [];
+var idsNaoVisitados = []
 
 var carregarIds = function (callback) {
     fs.readFile(fileName, function (err, data) {
         var json = JSON.parse(data)
         _keys = Object.keys(json)
         console.log('file loaded')
+        var arr = Object.keys(json).map(function(k) { return {id:k, visitado: json[k].visitado}} );
+        idsNaoVisitados = arr.filter(function(x) { return x.visitado == false}).map(function(x) {return x.id})
         _idsArquivo = json
         callback();
     });
@@ -31,20 +34,9 @@ var saveFile = function (callback) {
     }); 
 }
 var recuperarProximoId = function (req,res,next) {
-    var pos = _keys.indexOf(_idAtual);
-    if (pos == -1 ) {
-        for(var key of _keys){
-            if (_idsArquivo[key].visitado === false) {
-                _idAtual = key
-                break;
-            }
-        }
-        return _idAtual
-    }
-    if (pos + 1 == _keys.length){
-        console.log("Terminou")
-    }
-    _idAtual = _keys[pos+1]
+    var index = Math.floor(Math.random() * idsNaoVisitados.length)
+    _idAtual = idsNaoVisitados[index]
+    idsNaoVisitados.splice(index, 1);
     return _idAtual
 }
 
